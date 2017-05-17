eca555d74f38c8ed4ca4f56187b6c30b70dac130
muxi7 <1360097606@qq.com>
2017-05-17 20:34:12 +0800

    carousel 完成

--- plugin/js/carousel.js
+++ plugin/js/carousel.js
@@ -14,42 +14,47 @@
     this.timer=null;  //实现自动轮播的计时器；
     this.flag=false;  //判断是触发了mouseenter事件还是mouseleave()事件；解决自动轮播和手动切换的时候造成多余的setInternal;
 
-    this.picList.css({'width':this.picCount*this.picwidth});
-
-    this.preBtn.on('click',function(e){
-      e.preventDefault();
-      _this.playPre();
-    });
-    this.nextBtn.on('click',function(e){
-      e.preventDefault();
-      _this.playNext();
-    });
-    this.barList.children().on('click',function(){
-      var index=$(this).index();
-      var curIdx=_this.curIdx;
-      if(index>curIdx){
-        _this.playNext(index-curIdx);
-      }else{
-        _this.playPre(curIdx-index);
-      }
-
-    });
-
-    this.playAuto();
-
-    this.carousel.on('mouseenter',function(){
-      //e.stopPropagation();
-      _this.flag=true;
-      _this.stopAuto();
-    }).on('mouseleave',function(){
-      //e.stopPropagation();
-      _this.flag=false;
-      _this.playAuto();
-    });
-
+    this.init();
   }
 
   Carousel.prototype={
+    init:function(){
+      this.playAuto();
+      this.bind();
+    },
+  //事件绑定
+    bind:function(){
+      var _this=this;
+      this.picList.css({'width':this.picCount*this.picwidth});
+
+      this.preBtn.on('click',function(e){
+        e.preventDefault();
+        _this.playPre();
+      });
+      this.nextBtn.on('click',function(e){
+        e.preventDefault();
+        _this.playNext();
+      });
+
+      this.barList.children().on('click',function(){
+        var index=$(this).index();
+        var curIdx=_this.curIdx;
+        if(index>curIdx){
+          _this.playNext(index-curIdx);
+        }else{
+          _this.playPre(curIdx-index);
+        }
+      });
+
+      this.carousel.on('mouseenter',function(){
+        _this.flag=true;
+        _this.stopAuto();
+      }).on('mouseleave',function(){
+        _this.flag=false;
+        _this.playAuto();
+      });
+    },
+
   //向前滑动
     playPre:function(step){
       var _this=this;
@@ -106,6 +111,14 @@
     stopAuto:function(){ 
       window.clearInterval(this.timer);
     }
+  };
+
+//注册为jQuery插件
+  $.fn.Carousel=function(){
+    this.each(function(){
+      new Carousel($(this));
+    })
+    return this;
   };
 
   window.Carousel=Carousel;
